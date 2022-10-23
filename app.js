2d2c24c0f7a0b36d7ee52ed28b0bee36b69d0205
Abdulmohsen <a.abanmy113@gmail.com>
2022-10-24 00:09:09 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// https://avatars.dicebear.com/api/open-peeps/123123.svg?background=%230000ff
+
+backgroundClr = "3f3f3f"
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+})
+
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((el) => observer.observe(el));
