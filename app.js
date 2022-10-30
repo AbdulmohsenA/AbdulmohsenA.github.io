8df1f6d1bf7c8192543d970debabe0c095396e40
Abdulmohsen <a.abanmy113@gmail.com>
2022-10-30 23:47:13 +0300

    Bug Fix

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // https://avatars.dicebear.com/api/open-peeps/123123.svg?background=%230000ff
 function loadScript(url) {    
     var head = document.getElementsByTagName('head')[0];
@@ -7,6 +6,6 @@ function loadScript(url) {
     head.appendChild(script);
 }
 
-
 loadScript('./js/cardStager.js')
 loadScript('./js/observer.js')
+
--- js/cardStager.js
+++ js/cardStager.js
@@ -1,28 +1,29 @@
+const languages = [
+  ["Python", "./logos/Python-logo.png"],
+  ["JS", "./logos/JavaScript-logo.png"],
+  ["Java", "./logos/java-logo.png"],
+  ["SQL", "./logos/mysql.png"]]
+
+const sites = [
+  ["CV", "./logos/cv.png", ""],
+  ["Github", "./logos/git.png", "https://github.com/AbdulmohsenA"],
+  ["LinkedIn", "./logos/linin.png", "https://linkedin.com/"]]
+
+
 function cardStager(section, cardSet) {
-    cardsContainer = document.getElementById(section).getElementsByClassName("container")[0]
-    let cards = cardSet
-    for (let i = 0; i < cards.length; i++) {
-      let card = document.createElement("div")
-      card.className = "card hidden"
-      card.innerHTML = '<img src= "'+ cards[i][1] +'">'
-      if (cards[i].length > 2) {
-        card.innerHTML = '<a href="' + cards[i][2] + '" target="_blank"><img src= "'+ cards[i][1] +'"></a>'
-      }
-      card.style.transitionDelay = 200 * (i+1) + "ms"
-      cardsContainer.appendChild(card)
+  cardsContainer = document.getElementById(section).getElementsByClassName("container")[0]
+  let cards = cardSet
+  for (let i = 0; i < cards.length; i++) {
+    let card = document.createElement("div")
+    card.className = "card hidden"
+    card.innerHTML = '<img src= "'+ cards[i][1] +'">'
+    if (cards[i].length > 2) {
+      card.innerHTML = '<a href="' + cards[i][2] + '" target="_blank"><img src= "'+ cards[i][1] +'"></a>'
     }
+    card.style.transitionDelay = 200 * (i+1) + "ms"
+    cardsContainer.appendChild(card)
+  }
 }
 
-const languages = [
-    ["Python", "./logos/Python-logo.png"],
-    ["JS", "./logos/JavaScript-logo.png"],
-    ["Java", "./logos/java-logo.png"],
-    ["SQL", "./logos/mysql.png"]]
-  
-  const sites = [
-    ["CV", "./logos/cv.png", ""],
-    ["Github", "./logos/git.png", "https://github.com/AbdulmohsenA"],
-    ["LinkedIn", "./logos/linin.png", "https://linkedin.com/"]]
-  
-  cardStager("languages", languages)
-  cardStager("related", sites)
+cardStager("languages", languages)
+cardStager("related", sites)
