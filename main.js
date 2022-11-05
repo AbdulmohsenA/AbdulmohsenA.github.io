ddc6a986398120d5afef802bfe18ab360f0dc767
Abdulmohsen <a.abanmy113@gmail.com>
2022-11-05 17:32:11 +0300

    newPatch

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
 </section>
 
 <section id="languages">
-  <h2 class = "hidden">Proficient in</h2>
+  <h2 class = "hidden">Languages</h2>
   <div class="container"></div>
 </section>
 
@@ -24,21 +24,23 @@ document.querySelector('#app').innerHTML = `
 <div class="footimg"></div>
   </div>
 `
-sessionStorage.clear()
-
 // https://avatars.dicebear.com/api/open-peeps/123123.svg?background=%230000ff
 
-fetch("./data/data.json")
-  .then((response) => response.json())
-  .then((json) => {
-    let languages = json["languages"]
-    let sites     = json["sites"]
-    cardStager("languages", languages)
-    cardStager("related", sites)
-  })
+let languages = [
+  ["Python", "./logos/Python-logo.png"],
+  ["JS", "./logos/JavaScript-logo.png"],
+  ["Java", "./logos/java-logo.png"],
+  ["SQL", "./logos/mysql.png"]]
 
-window.onload = (e) => {
-  const hidden = document.querySelectorAll(".hidden");
+let sites = [
+  ["CV", "./logos/cv.png", ""],
+  ["Github", "./logos/git.png", "https://github.com/AbdulmohsenA"],
+  ["LinkedIn", "./logos/linin.png", "https://linkedin.com/"]]
+
+cardStager("languages", languages)
+cardStager("related", sites)
+
+window.onload = e => {
+  let hidden = document.querySelectorAll(".hidden");
   hidden.forEach((el) => observer.observe(el));
 }
-
