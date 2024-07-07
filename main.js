abec51964e37304eeaaf261c9fe659754307087f
AbdulmohsenA <a.abanmy3@gmail.com>
2024-07-07 16:16:08 +0300

    update

--- main.js
+++ main.js
@@ -32,8 +32,12 @@ document.querySelector('#app').innerHTML = `
 </section>
 
 <section id='related'>
-  <h2 class='hidden' data='🔗'>QR Code</h2>
-  <div class='container'><img src="./asset_files/CVQR.png" alt="QR Code" style="width: 40%; max-width: 40%; height:auto; display: block; margin-left: auto; margin-right: auto;"></div>
+  <h2 class='hidden' data='🔳'>CV</h2>
+  <div class='container'>
+  <a href="https://abdulmohsena.github.io/asset_files/CV.pdf" target="_blank>
+  <img src="./asset_files/CVQR.png" alt="QR Code" style="width: 40%; max-width: 40%; height:auto; display: block; margin-left: auto; margin-right: auto;" class='hidden'>
+  </a>
+  </div>
 </section>
 
 <div class='footimg'></div>
